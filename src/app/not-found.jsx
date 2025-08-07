6327dd9ea53b3c6c57eeffca55d81d591a16e085
vitor.argeri@aluno.senai.br <vitor.argeri@aluno.senai.br>
2025-08-07 13:49:17 -0300

    feat: add styles in not-found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,12 +4,11 @@ import styles from "./not-found.module.css"
 export default function NotFound() {
     return (
         <div className={styles.container}>
-            <h1>🔍 404 - Página não encontrada</h1>
-            <p>A página que você procura não existe.</p>
+            <h1 className={styles.title}>🔍 404 - Página não encontrada</h1>
+            <p className={styles.subtitle}>A página que você procura não existe.</p>
             
-            <div>
-                <Link href="/">🏠 Voltar para Home</Link>
-                <Link href="/contato">💬 Fale Conosco</Link>
+            <div className={styles.linkDiv}>
+                <Link href="/" className={styles.link}>🏠 Voltar para Home</Link>
             </div>
         </div>
     )
